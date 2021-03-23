121c548815b551e5ee89c0ae17c31f272f6f8fb3
Vlad <vlad.novikov@gmail.com>
2021-03-24 00:28:18 +0300

    added stepper

--- lib/room_screen.dart
+++ lib/room_screen.dart
@@ -18,6 +18,8 @@ String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
 void room_screen() => runApp(RoomScreen());
 
 class RoomScreen extends StatelessWidget {
+  int _currentStep = 0;
+  StepperType stepperType = StepperType.vertical;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,6 +49,9 @@ class _RoomPageState extends State<RoomPage> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int _currentStep = 0;
+  StepperType stepperType = StepperType.vertical;
 
   // TextField Controllers
   TextEditingController nameController = TextEditingController();
@@ -103,6 +108,67 @@ class _RoomPageState extends State<RoomPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
+
+            Expanded(
+              child: Stepper(
+                type: stepperType,
+                physics: ScrollPhysics(),
+                currentStep: _currentStep,
+                onStepTapped: (step) => tapped(step),
+                onStepContinue:  continued,
+                onStepCancel: cancel,
+                steps: <Step>[
+                  Step(
+                    title: new Text('Account'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Email Address'),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Password'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 0 ?
+                    StepState.complete : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Address'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Home Address'),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Postcode'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 1 ?
+                    StepState.complete : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Mobile Number'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Mobile Number'),
+                        ),
+                      ],
+                    ),
+                    isActive:_currentStep >= 0,
+                    state: _currentStep >= 2 ?
+                    StepState.complete : StepState.disabled,
+                  ),
+                ],
+              ),
+            ),
+
+
+
             Form(
                 key: _formKey,
                 child: Padding(
@@ -181,4 +247,24 @@ class _RoomPageState extends State<RoomPage> {
       ),
     );
   }
+
+  switchStepsType() {
+    setState(() => stepperType == StepperType.vertical
+        ? stepperType = StepperType.horizontal
+        : stepperType = StepperType.vertical);
+  }
+
+  tapped(int step){
+    setState(() => _currentStep = step);
+  }
+
+  continued(){
+    _currentStep < 2 ?
+    setState(() => _currentStep += 1): null;
+  }
+  cancel(){
+    _currentStep > 0 ?
+    setState(() => _currentStep -= 1) : null;
+  }
+
 }
